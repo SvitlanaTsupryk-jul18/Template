ce0f8d9fbdd69246ae63bd2d64eb68d19b917d14
Svet <stsupryk@gmail.com>
2019-07-06 04:35:31 +0300

    implement conter function

--- script.js
+++ script.js
@@ -2,6 +2,7 @@
     // invocation
     slider();
     burger();
+    counter();
 
     // Preloader();
     // sliderAbout();
@@ -17,7 +18,6 @@
     ///slider
 
     function slider() {
-        //Slider
 
         var slideIndex = 1;
         var timer = null;
@@ -29,13 +29,11 @@
         prev.addEventListener("click", plusSlides);
         next.addEventListener("click", plusSlides);
 
-        // Next/previous controls
         function plusSlides(event) {
             clearTimeout(timer);
             event.target === next ? n = 1 : n = -1;
             showSlides(slideIndex += n);
         }
-
 
         function showSlides(n) {
             var i;
@@ -90,49 +88,48 @@
             openbtn.classList.add("burger--visibility");
             menu.style.transform = ("translate(-500%)");
         }
-
-
     }
 
+    ////counter
 
-    //////preloader
+    function counter() {
+        window.addEventListener("scroll", handler);
 
-    function Preloader() {
-        document.body.onload = function () {
-            setTimeout(function () {
-                var preloader = document.querySelector(".preloader");
-                if (!preloader.classList.contains('done')) {
-                    preloader.classList.add('done');
-                }
-            }, 1000)
-        }
-    };
+        function handler() {
+            let container = document.querySelector(".counters");
+            let coords = container.getBoundingClientRect().top - container.offsetHeight - 100;
 
-    ////////// slider in about
-
-    function sliderAbout() {
-        var slwrapper = document.querySelector(".about__slider");
-        var slider = slwrapper.querySelector(".slider");
-        var prev = slwrapper.querySelector(".prev");
-        var next = slwrapper.querySelector(".next");
-        var slide = slider.querySelector(".slider__inner").offsetWidth;
-
-        var sdvig = 0;
-        slwrapper.addEventListener("click", function (event) {
-            if (event.target == next) {
-                sdvig -= slide;
-                if (sdvig < -slide * 4) {
-                    sdvig = 0;
-                }
-            } else if (event.target == prev) {
-                if (sdvig != 0) {
-                    sdvig += slide;
-                }
+            if (coords < 0) {
+                start();
+                this.removeEventListener("scroll", handler);
             }
-            slider.style.transform = 'translate(' + sdvig + 'px)';
-            console.log(sdvig);
-        });
+        }
+
+        function start() {
+            let counters = document.querySelectorAll(".counter__count");
+            let countersQuantity = counters.length;
+            let counter = [];
+
+            for (i = 0; i < countersQuantity; i++) {
+                counter[i] = parseInt(counters[i].innerHTML);
+            }
+
+            const count = function (start, value, id) {
+                let localStart = start;
+                setInterval(function () {
+                    if (localStart < value) {
+                        localStart++;
+                        counters[id].innerHTML = localStart;
+                    }
+                }, 10);
+            };
+
+            for (j = 0; j < countersQuantity; j++) {
+                count(0, counter[j], j);
+            }
+        }
     }
+
 
     /////////tabs in works
 
@@ -155,62 +152,6 @@
         };
     }
 
-    ////////slider in clients
-
-    // var slideIndex = 0;
-    // showSlides();
-
-    // function showSlides() {
-    //     var i;
-    //     var slides = document.getElementsByClassName("clients__slide");
-    //     var dots = document.getElementsByClassName("clients__dot");
-    //     for (i = 0; i < slides.length; i++) {
-    //         slides[i].style.display = "none";
-    //     }
-    //     slideIndex++;
-    //     if (slideIndex > slides.length) {
-    //         slideIndex = 1
-    //     }
-    //     for (i = 0; i < dots.length; i++) {
-    //         dots[i].className = dots[i].className.replace(" js-active", "");
-    //     }
-    //     slides[slideIndex - 1].style.display = "block";
-    //     dots[slideIndex - 1].className += " js-active";
-    //     setTimeout(showSlides, 2500); // Change image every 2 seconds
-    // }
-
-
-    //////buttons modal
-
-    function modal() {
-        let modalbtn = document.querySelector(".wellcome__btn");
-        let modalbtnPro = document.querySelector(".project__btn");
-
-        modalbtn.addEventListener("click", function () {
-            swal({
-                title: 'Hello! my Friend',
-                width: 600,
-                padding: '3em',
-                background: 'white',
-                confirmButtonColor: "#c0301c",
-                //swal.getContent ()
-                customClass: 'animated rubberBand'
-            });
-        });
-        modalbtnPro.addEventListener("click", function () {
-            swal({
-                title: 'Let\'s talk about YOUR PROJECT!',
-                type: "success",
-                width: 600,
-                padding: '3em',
-                background: 'white',
-                confirmButtonColor: "#c0301c",
-                confirmButtonText: "Good choice!",
-                showCancelButton: true,
-                cancelButtonText: "Later",
-            })
-        })
-    }
 
     ////smoothScroll
 
@@ -226,77 +167,5 @@
         });
     }
 
-    /// button to top
 
-    function toTop() {
-        var buttonTop = document.querySelector("#btn-up");
-        window.onscroll = function () {
-            if (document.body.scrollTop > 400 || document.documentElement.scrollTop > 400) {
-                buttonTop.style.opacity = "1";
-            } else {
-                buttonTop.style.opacity = "0";
-            }
-        }
-
-        buttonTop.addEventListener("click", function (e) {
-            e.preventDefault();
-            document.querySelector(".hero").scrollIntoView({
-                behavior: 'smooth'
-            });
-        })
-    }
-
-    /////Gsap
-
-    function Tween() {
-        let tl = new TimelineMax();
-        tl
-            .from('#left', 2, {
-                x: '-300%',
-                ease: Bounce.easeOut
-            }, delay = 1)
-            .from('#right', 2, {
-                x: '300%',
-                ease: Bounce.easeOut
-            }, "-=2")
-            .from('.wellcome__btn', .5, {
-                borderRadius: "30px"
-            })
-            .from('.logo__h1', 1, {
-                y: '-100',
-                opacity: 0
-            }, "-=0.5")
-            .staggerFrom(".nav__list", .5, {
-                y: '-50',
-                opacity: 0
-            }, 0.1, "-=0.25")
-    }
-
-    function scrollMagic() {
-        // init controller
-        var controller = new ScrollMagic.Controller();
-
-        var tween = TweenMax.from('#about', 1, {
-            y: 500,
-            opacity: 0
-        });
-        var tween1 = TweenMax.from('#contacts', 1, {
-            y: 500,
-            opacity: 0
-        });
-        // create a scene
-
-        var scene = new ScrollMagic.Scene({
-                triggerElement: "#servises",
-                offset: 400
-            })
-            .setTween(tween)
-            .addTo(controller);
-        var scene2 = new ScrollMagic.Scene({
-                triggerElement: "#clients",
-                offset: 300
-            })
-            .setTween(tween1)
-            .addTo(controller);
-    }
 })();
